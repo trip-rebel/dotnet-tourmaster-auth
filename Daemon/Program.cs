028be154a4d16a8be2c3563f872df9c758ec6b85
Pepijn Schoen <pepijn@eliandor.com>
2017-01-30 16:15:37 +0100

    Use new auth approach

--- Daemon/Program.cs
+++ Daemon/Program.cs
@@ -1,51 +1,39 @@
 ﻿using System;
 using System.Threading.Tasks;
-using System.Globalization;
-using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using IdentityModel.Client;
 
 namespace Daemon
 {
     public class Program
-    {//
-        // The Client ID is used by the application to uniquely identify itself to Azure AD.
-        // The App Key is a credential used by the application to authenticate to Azure AD.
-        // The Authority is the sign-in URL of the tenant.
-        //
-        private static string clientId = Environment.GetEnvironmentVariable("AD_CLIENTID");
-        private static string appKey = Environment.GetEnvironmentVariable("AD_APPKEY");
-        private static string authority = Environment.GetEnvironmentVariable("AD_AUTHORITY");
+    {
+        private static string tourMasterUsername = Environment.GetEnvironmentVariable("TM_USERNAME");
+        private static string tourMasterPassword = Environment.GetEnvironmentVariable("TM_PASSWORD");
 
         //
         // To authenticate to Tourmaster, the client needs to know the service's App ID URI.
         //
-        private static string tourMasterResourceId = Environment.GetEnvironmentVariable("TM_RESOURCEID");
+        private static string tourMasterAuthUrl = Environment.GetEnvironmentVariable("TM_AUTH_URL");
         private static string tourMasterUrl = Environment.GetEnvironmentVariable("TM_URL");
 
         private static HttpClient httpClient = new HttpClient();
-        private static AuthenticationContext authContext = null;
-        private static ClientCredential clientCredential = null;
 
         public static void Main(string[] args)
         {
-            Environment.SetEnvironmentVariable("AD_AUTHORITY", "https://login.microsoftonline.com/accounts.triprebel.com");
+            Environment.SetEnvironmentVariable("TM_AUTH_URL", "http://tourmaster.services.triprebel.com/auth/token");
             Environment.SetEnvironmentVariable("TM_URL", "http://tourmaster.services.triprebel.com/orders/test");
-            Environment.SetEnvironmentVariable("AD_CLIENTID", "CLIENT_ID");
-            Environment.SetEnvironmentVariable("AD_APPKEY", "APP_KEY");
-            Environment.SetEnvironmentVariable("TM_RESOURCEID", "633dca92-ffe8-4591-a9d4-c13e97445c31");
-
-            authContext = new AuthenticationContext(authority);
-            clientCredential = new ClientCredential(clientId, appKey);
+            Environment.SetEnvironmentVariable("TM_USERNAME", "username");
+            Environment.SetEnvironmentVariable("TM_PASSWORD", "password");
 
             TestUrl().Wait();
         }
 
         public static async Task TestUrl()
         {
-            var result = await GetToken();
+            var token = await GetToken();
 
-            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", result.AccessToken);
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
 
             var response = await httpClient.GetAsync(tourMasterUrl);
 
@@ -59,10 +47,17 @@ namespace Daemon
             }
         }
 
-        public static async Task<AuthenticationResult> GetToken()
+        public static async Task<string> GetToken()
         {
-            // ADAL includes an in memory cache, so this call will only send a message to the server if the cached token is expired.
-            return await authContext.AcquireTokenAsync(tourMasterResourceId, clientCredential);
+            var client = new TokenClient(
+                tourMasterAuthUrl,
+                "client-id",
+                "client-secret");
+            
+            var response = await client.RequestResourceOwnerPasswordAsync(tourMasterUsername, tourMasterPassword);
+            var token = response.AccessToken;
+
+            return token;
         }
     }
 }
